82bb9c6ab6d8c9cb3d87fadf078597cc61f0a405
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-09 07:22:36 -0300

    adds some helper libraries (fix torybook, eslint, prettier)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import WindiCSS from 'vite-plugin-windicss'
+import WindiCSS from "vite-plugin-windicss";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: "server",
   vite: {
-    plugins: [WindiCSS()]
-  }
+    plugins: [WindiCSS()],
+  },
 });
