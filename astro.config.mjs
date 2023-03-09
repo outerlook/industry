67ba87228477b92ae28a7679e0f2d599f3e04b39
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-09 08:01:58 -0300

    - try fix tinykeys

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import WindiCSS from "vite-plugin-windicss";
-import vercel from '@astrojs/vercel/serverless'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +10,8 @@ export default defineConfig({
   adapter: vercel(),
   vite: {
     plugins: [WindiCSS()],
+    ssr: {
+      noExternal: ["tinykeys"],
+    },
   },
 });
