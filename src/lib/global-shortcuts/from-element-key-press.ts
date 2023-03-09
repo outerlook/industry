aa68f0047444544aeb8c6f618833937a15191a30
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-09 07:58:11 -0300

    - try fix tinykeys

--- src/lib/global-shortcuts/from-element-key-press.ts
+++ src/lib/global-shortcuts/from-element-key-press.ts
@@ -1,11 +1,14 @@
-import {Observable} from "rxjs";
-import {createKeybindingsHandler} from "tinykeys";
+import { Observable } from "rxjs";
+import * as tk from "tinykeys";
 
-export const fromElementKeyPress = (el: HTMLElement | Document, keybinding: string) =>
-    new Observable<KeyboardEvent>((subscriber) => {
-        const handler = createKeybindingsHandler({
-            [keybinding]: (event) => subscriber.next(event),
-        });
-        el.addEventListener("keydown", handler);
-        return () => el.removeEventListener("keydown", handler);
+export const fromElementKeyPress = (
+  el: HTMLElement | Document,
+  keybinding: string
+) =>
+  new Observable<KeyboardEvent>((subscriber) => {
+    const handler = tk.createKeybindingsHandler({
+      [keybinding]: (event) => subscriber.next(event),
     });
+    el.addEventListener("keydown", handler);
+    return () => el.removeEventListener("keydown", handler);
+  });
