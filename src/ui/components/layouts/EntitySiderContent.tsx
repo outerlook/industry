93e3b9c6f26554c0284c9ffcb5ecc76c7568d759
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-11 19:50:53 -0300

    Arruma o sider das páginas de entidades

--- src/ui/components/layouts/EntitySiderContent.tsx
+++ src/ui/components/layouts/EntitySiderContent.tsx
@@ -1,3 +1,21 @@
+import { Menu } from "antd";
+import { servicesList } from "@/ui/components/common/servicos/services-list";
+
 export function EntitySiderContent() {
-    return null;
+  let initialSelected =
+    typeof window !== "undefined" ? window.location.pathname : "";
+  return (
+    <Menu
+      defaultSelectedKeys={[initialSelected]}
+      items={[
+        { label: "Lists", type: "group", key: "services" },
+        ...servicesList.map((s) => ({
+          label: <a href={s.href}>{s.label}</a>,
+          key: s.href,
+          title: s.label,
+          icon: <s.Icon />,
+        })),
+      ]}
+    ></Menu>
+  );
 }
