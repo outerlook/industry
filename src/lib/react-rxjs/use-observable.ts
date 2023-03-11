7ff905395a3557e0d8fc8d218b1b369f8c4c3340
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-11 09:38:15 -0300

    todo to improve useObservable

--- src/lib/react-rxjs/use-observable.ts
+++ src/lib/react-rxjs/use-observable.ts
@@ -1,15 +1,17 @@
 import React from "react";
-import type {Observable} from "rxjs";
+import type { Observable } from "rxjs";
 
-export const useObservable = <T>( observable: Observable<T> ) => {
-    const [state, setState] = React.useState<T>();
+export const useObservable = <T>(observable: Observable<T>) => {
+  const [state, setState] = React.useState<T>();
 
-    React.useEffect(() => {
-        const sub = observable.subscribe(setState);
-        return () => {
-            sub.unsubscribe();
-        };
-    }, [observable]);
+  React.useEffect(() => {
+    const sub = observable.subscribe(setState);
+    return () => {
+      sub.unsubscribe();
+    };
+  }, [observable]);
 
-    return state;
+  // TODO: Could get fancier and return a loading state to leverage Suspense
+
+  return state;
 };
