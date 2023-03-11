e86a61c5f380be3a162ddc3582a49af95cd9f621
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-11 17:09:10 -0300

    adds user menu to header

--- src/ui/components/common/Header/AppHeader.tsx
+++ src/ui/components/common/Header/AppHeader.tsx
@@ -1,11 +1,15 @@
-import {Layout, Space} from "antd";
-import {SiteLogo} from "../SiteLogo";
-import {SearchBar} from "@/ui/components/common/Header/SearchBar";
-import {ServicesButton} from "@/ui/components/common/Header/ServicesButton";
+import { Button, Layout, Space } from "antd";
+import { SiteLogo } from "../SiteLogo";
+import { SearchBar } from "@/ui/components/common/Header/SearchBar";
+import { ServicesButton } from "@/ui/components/common/Header/ServicesButton";
+import { LoggedMenu } from "@/ui/components/common/Header/LoggedMenu/LoggedMenu";
+import { QuestionOutlined } from "@ant-design/icons";
+import {notImplementedHalMsg} from "@/lib/utils/not-implemented";
 
 const { Header } = Layout;
 
 export const AppHeader = () => {
+  const isLoggedIn = true; // replaceable
   return (
     <Layout className={"flex-none"}>
       <Header className="bg-gray-800 sticky h-12 fixed w-full">
@@ -17,12 +21,14 @@ export const AppHeader = () => {
             <ServicesButton></ServicesButton>
             <SearchBar />
           </Space>
-          <div className="flex items-center">
-            <div className="text-white text-xl font-bold">User</div>
-          </div>
+          <Space>
+            <Button onClick={notImplementedHalMsg('find help')} className={'bg-transparent border-none text-white'} shape={"circle"}>
+              <QuestionOutlined />
+            </Button>
+            {isLoggedIn ? <LoggedMenu /> : "oh no"}
+          </Space>
         </div>
       </Header>
     </Layout>
   );
 };
-
---
+++ src/ui/components/common/Header/LoggedMenu/LoggedMenu.tsx
@@ -0,0 +1,15 @@
+import { LoggedDropdown } from "@/ui/components/common/Header/LoggedMenu/LoggedDropdown";
+import { Avatar, Button } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+export const LoggedMenu = () => {
+  return (
+    <LoggedDropdown>
+      <Button
+        className={"bg-slate-600 text-white"}
+        shape={"circle"}
+        icon={<UserOutlined />}
+      ></Button>
+    </LoggedDropdown>
+  );
+};
