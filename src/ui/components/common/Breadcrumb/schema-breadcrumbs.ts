3c549a653e7632ecf173135ac0132cfb24dbb07e
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-12 18:23:34 -0300

    fix get breadcrumb not getting for asst

--- src/ui/components/common/Breadcrumb/schema-breadcrumbs.ts
+++ src/ui/components/common/Breadcrumb/schema-breadcrumbs.ts
@@ -1,6 +1,6 @@
-import {linkTo} from "../../../../meta/__GENERATED__/routes";
-import type {ItemType} from "antd/es/breadcrumb/Breadcrumb";
-import type {validTypes} from "../../../../lib/io-ts/valid-types";
+import { linkTo } from "../../../../meta/__GENERATED__/routes";
+import type { ItemType } from "antd/es/breadcrumb/Breadcrumb";
+import type { validTypes } from "../../../../lib/io-ts/valid-types";
 
 // change proof
 export type BreadcrumbItem = ItemType;
@@ -15,52 +15,68 @@ const forCompanies = ({ company }: { company: validTypes["Company"] }) => {
   ];
 };
 
-const forUnits = ({ unit, ...rest }: { unit: validTypes["Unit"] } & Parameters<typeof forCompanies>[0]) => {
-    return [
-        ...forCompanies(rest),
-        { title: "Units", href: linkTo["/units"]({}) },
-        {
-        title: unit.name,
-        href: linkTo["/units/:id"]({ id: unit.id.toString() }),
-        },
-    ];
-}
+const forUnits = ({
+  unit,
+  ...rest
+}: { unit: validTypes["Unit"] } & Parameters<typeof forCompanies>[0]) => {
+  return [
+    ...forCompanies(rest),
+    { title: "Units", href: linkTo["/units"]({}) },
+    {
+      title: unit.name,
+      href: linkTo["/units/:id"]({ id: unit.id.toString() }),
+    },
+  ];
+};
 
-const forAssets = ({ asset, ...rest }: { asset: validTypes["Asset"] } & Parameters<typeof forUnits>[0]) => {
-    return [
-        ...forUnits(rest),
-        { title: "Assets", href: linkTo["/assets"]({}) },
-        { title: asset.name },
-    ];
-}
+const forAssets = ({
+  asset,
+  ...rest
+}: { asset: validTypes["Asset"] } & Parameters<typeof forUnits>[0]) => {
+  return [
+    ...forUnits(rest),
+    { title: "Assets", href: linkTo["/assets"]({}) },
+    {
+      title: asset.name,
+      href: linkTo["/assets/:id"]({ id: asset.id.toString() }),
+    },
+  ];
+};
 
-const forUsers = ({ user, ...rest }: { user: validTypes["User"] } & Parameters<typeof forUnits>[0]) => {
-    return [
-        ...forUnits(rest),
-        { title: "Users", href: linkTo["/users"]({}) },
-        {
-        title: user.name,
-        href: linkTo["/users/:id"]({ id: user.id.toString() }),
-        },
-    ];
-}
+const forUsers = ({
+  user,
+  ...rest
+}: { user: validTypes["User"] } & Parameters<typeof forUnits>[0]) => {
+  return [
+    ...forUnits(rest),
+    { title: "Users", href: linkTo["/users"]({}) },
+    {
+      title: user.name,
+      href: linkTo["/users/:id"]({ id: user.id.toString() }),
+    },
+  ];
+};
 
-const forWorkorders = ({ workorder, ...rest }: { workorder: validTypes["Workorder"] } & Parameters<typeof forAssets>[0]) => {
-    return [
-        ...forAssets(rest),
-        { title: "Workorders", href: linkTo["/workorders"]({}) },
-        {
-        title: workorder.title,
-        href: linkTo["/workorders/:id"]({ id: workorder.id.toString() }),
-        },
-    ];
-}
+const forWorkorders = ({
+  workorder,
+  ...rest
+}: {
+  workorder: validTypes["Workorder"];
+} & Parameters<typeof forAssets>[0]) => {
+  return [
+    ...forAssets(rest),
+    { title: "Workorders", href: linkTo["/workorders"]({}) },
+    {
+      title: workorder.title,
+      href: linkTo["/workorders/:id"]({ id: workorder.id.toString() }),
+    },
+  ];
+};
 
-
-export const  getBreadcrumb = {
-    forCompanies,
-    forUnits,
-    forAssets,
-    forUsers,
-    forWorkorders
-}
+export const getBreadcrumb = {
+  forCompanies,
+  forUnits,
+  forAssets,
+  forUsers,
+  forWorkorders,
+};
