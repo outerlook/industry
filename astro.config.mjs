3c44a63d6d6edd8699c3eb5f0c1f856676209729
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-13 15:05:07 -0300

    try fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ const noExternal = ["tinykeys", "fp-ts-rxjs", "fp-ts"];
 // Don't use nullish coalescing operator here
 // Vite doesn't support it yet as import.meta.env is not a real object
 // but this check is needed to make it work on vitest config
-if (import.meta.env && import.meta.env.PROD) {
+if (import.meta.env.PROD) {
   // won't work locally
   noExternal.push("mappersmith");
 }
