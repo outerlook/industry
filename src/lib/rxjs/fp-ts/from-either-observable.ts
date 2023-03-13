54458b88b9606acb758c7e88108b68ba321b7485
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-13 11:50:34 -0300

    some refactors

--- src/lib/rxjs/fp-ts/from-either-observable.ts
+++ src/lib/rxjs/fp-ts/from-either-observable.ts
@@ -1,13 +1,13 @@
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 import * as E from "fp-ts/Either";
-import {pipe} from "effect";
+import { pipe } from "effect";
 import * as OE from "fp-ts-rxjs/ObservableEither";
-import {toUnknownError} from "@/lib/errors/unknown-error";
+import { toUnknownError } from "@/lib/errors/unknown-error";
 
 export const fromEitherObservable = <A, Err>(o: Observable<E.Either<Err, A>>) =>
-    pipe(
-        OE.fromObservable(o),
-        // unknown error -> Error to preserve this information
-        OE.mapLeft(toUnknownError),
-        OE.chainW(OE.fromEither)
-    );
+  pipe(
+    OE.fromObservable(o),
+    // unknown error -> Error to preserve this information
+    OE.mapLeft(toUnknownError),
+    OE.chainW(OE.fromEither)
+  );
