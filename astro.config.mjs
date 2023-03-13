4dfe9cb8584ad1da51b6709b2d5aea229302cb64
Raffael Campos <raffael.rcampos@gmail.com>
2023-03-13 15:02:14 -0300

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import vercel from "@astrojs/vercel/serverless";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const noExternal = ["tinykeys", "fp-ts-rxjs", "fp-ts"];
-if (import.meta.env?.PROD) {
+
+// Don't use nullish coalescing operator here
+// Vite doesn't support it yet as import.meta.env is not a real object
+// but this check is needed to make it work on vitest config
+if (import.meta.env && import.meta.env.PROD) {
   // won't work locally
   noExternal.push("mappersmith");
 }
